4b657b2b1d9cb29c338c7e575002678d28cd3f03
Woosun Jang <jin890@yonsei.ac.kr>
2018-05-01 23:59:12 +0900

    test commit for setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ws_py',
+    version='1.0',
+    packages=['', 'IO', 'struc', 'utils', 'cmdline', 'Parsers', 'Plotter',
+              'boltztrap', 'polyhedron', 'ElectronicStructure'],
+    url='https://github.com/materials-theory/ws_py',
+    license='GPL v3',
+    author='Woosun Jang',
+    author_email='jin890@yonsei.ac.kr',
+    description='A set of python libraries to handle the VASP input / output files'
+)
