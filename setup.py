a7166dafc4513af96ce87f5896d7b280d250cf21
Woosun Jang <jin890@yonsei.ac.kr>
2018-09-10 14:01:37 +0900

    new setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='vw-master',
+    version='',
+    packages=['tests', 'vw_py', 'vw_py.IO', 'vw_py.struc', 'vw_py.cmdline', 'vw_py.Parsers',
+              'vw_py.Plotter', 'vw_py.boltztrap', 'vw_py.polyhedron', 'vw_py.generalutils',
+              'vw_py.ElectronicStructure', 'examples'],
+    url='https://github.com/materials-theory/ws_py',
+    license='GPL 3.0',
+    author='Woosun Jang',
+    author_email='jin890@yonsei.ac.kr',
+    description='Python script to post-process VASP generated files'
+)
